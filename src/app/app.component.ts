1d3ec2d7172637dbf7f9b8dc5051a7a7d4a18f32
FernandoSBento <fernando_bento2@hotmail.com>
2019-02-28 22:32:58 -0300

    exercicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import  { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'site';
+export class AppComponent{
+  nome: string = "Fernando";
+  valor: string = "";
+  n1: string ="";
+  n2: string ="";
+
+
+
+
+  soma(){
+  total: parseInt(this.n1) + parseInt(this.n2);
+
+  }
+
+
+
+
+  ApresentaNome(valor){
+    alert('Boa noite ' + this.valor);
+  }
+
+
 }
